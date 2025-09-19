20f2f234324e6fead9a569b5953c5e938a19513f
Patata-Kun <jeremias.cardona.b@gmail.com>
2025-09-19 11:46:03 -0300

    v2.4.0
    Changelog
    - Change categoriesCars.html name to products.html
    - Add product-info.html
    - Add categories.html
    - Add constants and variables for API calls
    - Implement API calls for categories information
    - Other things

--- js/categories.js
+++ js/categories.js
@@ -1,5 +1,5 @@
-// init de las constantes necesarias para la API
-const categoriesURL = "https://patata-kun.github.io/e-mercado-API/cats/cat_json"
+// init de las constantes necesarias para la API (llamadas a la API)
+const categoriesURL = "https://patata-kun.github.io/e-mercado-API/cats/cat.json"
 
 const productsURL = "https://patata-kun.github.io/e-mercado-API/cats_products/"
 const productInfoURL = "https://patata-kun.github.io/e-mercado-API/products/"
@@ -9,55 +9,118 @@ const publishProductURL  = "https://patata-kun.github.io/e-mercado-API/sell/publ
 const cartInfoURL = "https://patata-kun.github.io/e-mercado-API/user_cart/"
 const cartBuyURL = "https://patata-kun.github.io/e-mercado-API/cart/buy.json"
 
-// esta función asocia un ícono según el tipo de auto (car o suv)
-function getCarIconPath(iconType) {
-  const iconPaths = {
-    'car': 'M240 104H229.2L201.42 41.5A16 16 0 00186.8 32H69.2A16 16 0 0054.58 41.5L26.8 104H16a8 8 0 000 16h8v80a16 16 0 0016 16H64a16 16 0 0016-16v-8h96v8a16 16 0 0016 16h24a16 16 0 0016-16V120h8a8 8 0 000-16ZM80 152H56a8 8 0 010-16H80a8 8 0 010 16Zm120 0H176a8 8 0 010-16h24a8 8 0 010 16ZM44.31 104 69.2 48H186.8l24.89 56Z',
-    'suv': 'M248 103.47A8.17 8.17 0 00239.73 96H232a8 8 0 00-.18-1.68L221.18 44.65A16.08 16.08 0 00205.53 32H50.47A16.08 16.08 0 0034.82 44.65L24.18 94.32A8 8 0 0024 96H16.27A8.17 8.17 0 008 103.47 8 8 0 0016 112h8v88a16 16 0 0016 16H64a16 16 0 0016-16V184h20a4 4 0 004-4V128.27a8.17 8.17 0 017.47-8.25 8 8 0 018.53 8v52a4 4 0 004 4h8a4 4 0 004-4V128.27a8.17 8.17 0 017.47-8.25 8 8 0 018.53 8v52a4 4 0 004 4h20v16a16 16 0 0016 16h24a16 16 0 0016-16V112h8a8 8 0 008-8.53ZM68 144a12 12 0 1112-12A12 12 0 0168 144Zm120 0a12 12 0 1112-12 12 12 0 01-12 12ZM40.18 96 50.47 48H205.53l10.29 48Z'
-  };
-  return iconPaths[iconType] || null;
+// init de las constantes y variables necesarias para el filtrado y ordenamiento de categorías
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const ORDER_BY_PROD_COUNT = "Cant.";
+let currentCategoriesArray = [];
+let currentSortCriteria = undefined;
+let minCount = undefined;
+let maxCount = undefined;
+
+// Función para obtener datos JSON
+function getJSONData(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then(data => {
+      return {
+        status: "ok",
+        data: data
+      };
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      return {
+        status: "error",
+        data: null
+      };
+    });
 }
 
-// // fetch de carga del título de la categoría específica
-// const categoryId = localStorage.getItem('catID') || null;
+// función de ordenamiento de categorías
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.name < b.name ){ return -1; }
+            if ( a.name > b.name ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_NAME){
+        result = array.sort(function(a, b) {
+            if ( a.name > b.name ){ return -1; }
+            if ( a.name < b.name ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.productCount);
+            let bCount = parseInt(b.productCount);
 
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
 
-// fetch(categoriesURL)
-//   .then(response => response.json())
-//   .then(data => {
-//     const categoriesTitle = document.querySelector('.categories-title');
-//     if (categoriesTitle) {
-//       const selectedCategory = data.find(category => category.id == categoryId);
-      
-//       if (selectedCategory) {
-//         categoriesTitle.innerHTML = `
-//           <div class="category-item">
-//             <h1>${selectedCategory.name}</h1>
-//             <p>${selectedCategory.description}</p>
-//           </div>
-//         `;
-//       } else {
-//         console.warn(`Categoría con ID ${categoryId} no encontrada`);
-//       }
-//     }
-//   })
-//   .catch(error => console.error('Error al cargar títulos:', error));
-  
+    return result;
+}
 
-// fetch de carga de las categorías mostradas en categories.html
-fetch (categoriesURL)
-  .then(response => response.json())
-  .then(data => {
-    const categoriesList = document.querySelector('.categories-container');
-  })
-  .catch(error => console.error('Error al cargar las categorías:', error))
+// función para setear el id de la categoría seleccionada y redirigir a products.html
+function setCatID(id) {
+  localStorage.setItem('catID', id);
+  window.location = "products.html";
+}
 
+function showCategories() {
+  let htmlCategoriesContent = "";
+  for(let i = 0; i < currentCategoriesArray.length; i++){
+      let category = currentCategoriesArray[i];
 
+            htmlCategoriesContent += `
+            <div onclick="setCatID(${category.id})" class="categories-product">
+              <img class="product-image" src="${category.imgSrc}" alt="${category.description}">
+              <div class="product-description">
+                <h2 class="product-name"> ${category.name} </h2>
+                <p>${category.description}</p>
+                <div class="product-price-sold">
+                  <p>${category.productCount} vendidos</p>
+                </div>
+              </div>
+            </div>
+            `;
+  }
+  document.getElementById("categories-list").innerHTML = htmlCategoriesContent;
+}
 
+document.addEventListener('DOMContentLoaded', function(e) {
+  getJSONData(categoriesURL).then(function(resultObj) {
+    if (resultObj.status === "ok") {
+      currentCategoriesArray = resultObj.data;
+      showCategories();
+    }
+});
 
+});
 
+function sortAndShowCategories(sortCriteria, categoriesArray){
+    currentSortCriteria = sortCriteria;
 
+    if(categoriesArray != undefined){
+        currentCategoriesArray = categoriesArray;
+    }
 
+    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+    showCategories();
+}
 
 
 // fetch de carga de los productos de la categoría específica (por ahora es solo de autos [101.json])
@@ -96,4 +159,13 @@ fetch(productsURL + '/101.json')
       wrapper.appendChild(carDiv);
     });
   })
-  .catch(error => console.error('Error al cargar la categoría:', error));
+  .catch(error => console.error('Error loading category:', error));
+
+// esta función asocia un ícono según el tipo de auto (car o suv)
+function getCarIconPath(iconType) {
+  const iconPaths = {
+    'car': 'M240 104H229.2L201.42 41.5A16 16 0 00186.8 32H69.2A16 16 0 0054.58 41.5L26.8 104H16a8 8 0 000 16h8v80a16 16 0 0016 16H64a16 16 0 0016-16v-8h96v8a16 16 0 0016 16h24a16 16 0 0016-16V120h8a8 8 0 000-16ZM80 152H56a8 8 0 010-16H80a8 8 0 010 16Zm120 0H176a8 8 0 010-16h24a8 8 0 010 16ZM44.31 104 69.2 48H186.8l24.89 56Z',
+    'suv': 'M248 103.47A8.17 8.17 0 00239.73 96H232a8 8 0 00-.18-1.68L221.18 44.65A16.08 16.08 0 00205.53 32H50.47A16.08 16.08 0 0034.82 44.65L24.18 94.32A8 8 0 0024 96H16.27A8.17 8.17 0 008 103.47 8 8 0 0016 112h8v88a16 16 0 0016 16H64a16 16 0 0016-16V184h20a4 4 0 004-4V128.27a8.17 8.17 0 017.47-8.25 8 8 0 018.53 8v52a4 4 0 004 4h8a4 4 0 004-4V128.27a8.17 8.17 0 017.47-8.25 8 8 0 018.53 8v52a4 4 0 004 4h20v16a16 16 0 0016 16h24a16 16 0 0016-16V112h8a8 8 0 008-8.53ZM68 144a12 12 0 1112-12A12 12 0 0168 144Zm120 0a12 12 0 1112-12 12 12 0 01-12 12ZM40.18 96 50.47 48H205.53l10.29 48Z'
+  };
+  return iconPaths[iconType] || null;
+}
