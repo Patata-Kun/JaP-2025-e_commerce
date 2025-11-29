3714f4a2dc609468b7f9a84fd0c2cfb6ecc43a97
Leonardo Farias <leofarias.20@hotmail.com>
2025-11-29 12:12:51 -0300

    v12.15.1

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const jwt = require("jsonwebtoken"); //AQUÍ LE PIDO A NODE.JS QUE IMPORTE LA LIBRERÍA jsonwebtoken Y QUE DESPUÉS LA GUARDE EN UNA VARIABLE.
 
 const app = express();
 const PORT = 3000;
 
 // PERMITIR PETICIONES DEL FRONTEND
 app.use(cors());
+app.use(express.json()); //ESTO PERMITE QUE express PUEDA ENTENDER/LEER LO QUE LE MANDO DESDE EL FRONTEND. SIN EL express.json() EL BACKEND NO LO ENTIENDE, SIN EMBARGO CON, LO CONVIERTE EN UN OBJETO Y LO PUEDO USAR. 
+
 
 // SERVIR TODA LA CARPETA /data COMO API
 app.use("/api", express.static(path.join(__dirname, "data")));
+
+
+
+const users = [ //ACÁ CREÉ UNA BASE DE DATOS PEQUEÑA Y LE DI ALGUNOS NOMBRES DE USUARIO Y CONTRASEÑA PARA PROBAR INICIAR SESIÓN CON ELLOS.
+  { id: 1, username: "jeremias", password: "12345" },
+  { id: 2, username: "leo", password: "12345" },
+  { id: 3, username: "jazmin", password: "12345"},
+  { id: 4, username: "kenny", password:"12345"},
+  { id: 5, username: "naty", password: "12345"}
+]; //CUANDO PRUEBO INICIAR SESIÓN SI LOS DATOS QUE INGRESO SON CORRESPONDIENTES A ESTA BASE DE DATOS, LOGRO HACER LOGIN, SINO NO.
+
+
+
+app.post("/login", (req, res) => { //CUANDO INTENTE HACER LOGIN.
+  const { username, password } = req.body; //req.body VA A EXTRAER LAS PROPIEDADES DE USUARIO Y CONTRASEÑA DESDE EL OBJETO.
+
+  const user = users.find( // BUSCA ENTONCES QUE EL USUARIO Y LA CONTRASEÑA COINCIDAN.
+    (u) => u.username === username && u.password === password
+  );
+
+  if (!user) { //SI LOS DATOS DE USUARIO INGRESADOS NO COINCIDEN CON UNO DENTRO DE MI BASE DE DATOS, ME DA ERROR.
+    return res.status(401).json({ message: "Usuario o contraseña incorrectos" });
+  }
+
+  const token = jwt.sign( // SI COINCIDEN, SE GENERA EL TOKEN, UN TOKEN QUE TIENE userId y Username
+    { userId: user.id, username: user.username },
+    "CLAVE_SECRETA", //SIRVE COMO UNA ESPECIE DE FIRMA O SELLO EN CADA TOKEN Y ASÍ, ES VALIDO, SOLO EL SERVDIDOR LO CONOCE.
+    { expiresIn: "2h" } //EL TOKEN TENDRÁ UNA DURABILIDAD DE 2 HORAS, DESPUÉS DE ESTE TIEMPO EL USUARIO TIENE QUE HACER LOGIN OTRA VEZ.
+  );
+
+  res.json({ token }); //LE DEVUELVO AL CLIENTE, EL TOKEN.
+});
+
 
 // PROBAR SERVIDOR
 app.get("/", (req, res) => {
@@ -20,3 +56,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Servidor levantado en http://localhost:" + PORT);
 });
+
+
